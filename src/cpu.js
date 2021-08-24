3afa23a90a5108d027031776b96f80173f608979
wake-code <87005812+wake-code@users.noreply.github.com>
2021-08-24 21:00:31 +0300

    Basic CPU Structure

---
+++ src/cpu.js
@@ -0,0 +1,17 @@
+const createMemory = require("./create-memory")
+
+class CPU {
+    constructor(memory) {
+        this.memory = memory
+
+        this.registerNames = [
+            'ip', 'acc',
+            'r1', 'r2', 'r3', 'r4',
+            'r5', 'r6', 'r7', 'r8'
+        ]
+
+        this.registers = createMemory(this.registerNames.length * 2)
+    }
+}
+
+module.exports = CPU
