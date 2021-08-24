f8bbec3da33a2eb8a305c90475c5bc151b9e3a5a
wake-code <87005812+wake-code@users.noreply.github.com>
2021-08-24 21:18:54 +0300

    Execute

--- src/cpu.js
+++ src/cpu.js
@@ -43,6 +43,46 @@ class CPU {
 
         return instruction
     }
+
+    fetch16() {
+        const nextInstructionAddress = this.getRegister('ip')
+        const instruction = this.memory.getUint16(nextInstructionAddress)
+
+        this.setRegister('ip', nextInstructionAddress + 2)
+
+        return instruction
+    }
+
+    execute(instruction) {
+        switch (instruction) {
+            case 0x10: { // Move literal into r1 register
+                const literal = this.fetch16()
+                
+                this.setRegister('r1', literal)
+
+                return
+            }
+
+            case 0x11: { // Move literal into r2 register
+                const literal = this.fetch16()
+                
+                this.setRegister('r2', literal)
+
+                return
+            }
+
+            case 0x12: { // Add register to register
+                const r1 = this.fetch()
+                const r2 = this.fetch()
+                const registerValue1 = this.registers.getUint16(r1 * 2);
+                const registerValue2 = this.registers.getUint16(r2 * 2);
+
+                this.setRegister('acc', registerValue1 + registerValue2)
+                
+                return
+            }
+        }
+    }
 }
 
 module.exports = CPU
